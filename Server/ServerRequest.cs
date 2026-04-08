e7dc753dc2a18ba65a20b2b3707f069204c7d2c9
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2026-04-08 11:54:34 +0300

    Добавьте файлы проекта.

---
+++ Server/ServerRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleSixApp2.Server
+{
+    public class ServerRequest
+    {
+        public async Task<string> GetRequest(string url)
+        {
+            using (HttpClient client = new HttpClient())
+            {
+                HttpResponseMessage responseMessage = await client.GetAsync(url);
+                if(responseMessage.IsSuccessStatusCode)
+                {
+                    return await responseMessage.Content.ReadAsStringAsync();
+                }
+                else
+                {
+                    return $"Ошибка: {responseMessage.StatusCode}";
+                }
+            }
+        }
+    }
+}
---
+++ View/ValidationWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using ModuleSixApp2.Server;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ModuleSixApp2.View
+{
+    /// <summary>
+    /// Логика взаимодействия для ValidationWindow.xaml
+    /// </summary>
+    public partial class ValidationWindow : Window
+    {
+        public ValidationWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async Task Get_Data_Button_Click(object sender, RoutedEventArgs e)
+        {
+            string url = "http://localhost:4444/TransferSimulator/fullName";
+
+            string data = await new ServerRequest().GetRequest(url);
+
+            DataTextBlock.Text = data;
+        }
+    }
+}
