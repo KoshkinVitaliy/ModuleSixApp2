0f1e5709ba9ec13f71e9f79cc28baa5513c99a8c
KoshkinVitaliy <kosh98vitaliy@outlook.com>
2026-04-08 14:30:56 +0300

    v2

---
+++ Server/WordWriter.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Word = Microsoft.Office.Interop.Word;
+
+namespace ModuleSixApp2.Server
+{
+    public class WordWriter
+    {
+        private static int rowIndex = 3;
+
+        public static void WriteToWord(
+            string filePath,
+            int tableIndex,
+            int columnIndex,
+            string data
+            )
+        {
+            Word.Application application = new Word.Application();
+            application.Visible = false;
+
+            try
+            {
+                Word.Document document = application.Documents.Open(filePath);
+                Word.Table table = document.Tables[tableIndex];
+
+                if(columnIndex < 1 || columnIndex > table.Columns.Count)
+                {
+                    MessageBox.Show("Неправильный индекс колонки!");
+                }
+                else if(rowIndex <= table.Rows.Count)
+                {
+                    table.Cell(rowIndex, columnIndex).Range.Text = data;
+                    rowIndex++;
+                }
+                else
+                {
+                    table.Rows.Add();
+                    table.Cell(rowIndex, columnIndex).Range.Text = data;
+                    rowIndex++;
+                }
+                document.Save();
+                document.Close();
+
+                MessageBox.Show("Результаты теста успешно записаны!");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                application.Quit();
+            }
+        }
+    }
+}
--- View/ValidationWindow.xaml.cs
+++ View/ValidationWindow.xaml.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -25,13 +26,62 @@ namespace ModuleSixApp2.View
             InitializeComponent();
         }
 
-        private async Task Get_Data_Button_Click(object sender, RoutedEventArgs e)
+        private async void Get_Data_Button_Click(object sender, RoutedEventArgs e)
         {
             string url = "http://localhost:4444/TransferSimulator/fullName";
 
             string data = await new ServerRequest().GetRequest(url);
 
-            DataTextBlock.Text = data;
+            DataTextBlock.Text = GetDataFromJSON(data);
+        }
+
+        private string GetDataFromJSON(string data)
+        {
+            return data
+                .Substring(data.IndexOf(":") + 2)
+                .Replace("\"", "")
+                .Replace("}", "");
+        }
+
+        private void Send_Result_Button_Click(object sender, RoutedEventArgs e)
+        {
+            if(DataTextBlock.Text.Equals(""))
+            {
+                MessageBox.Show("Данные с сервера ещё не получены!");
+            }
+            else
+            {
+                //записывать результаты теста
+                string fullNamePattern = @"^[А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+ [А-ЯЁ][а-яё]+$";
+
+                CheckRegex(fullNamePattern, DataTextBlock.Text);
+            }
+        }
+
+        private void CheckRegex(string fullNamePattern, string text)
+        {
+            if(!Regex.IsMatch(text, fullNamePattern))
+            {
+                WordWriter.WriteToWord(
+                  "C:\\Users\\admin\\source\\repos\\ModuleSixApp2\\ТестКейс.docx",
+                  1,
+                  1,
+                  DataTextBlock.Text);
+
+                ResultTextBlock.Text = "Данные ФИО провалили валидацию!";
+                ResultTextBlock.Foreground = Brushes.Red;
+            }
+            else
+            {
+                WordWriter.WriteToWord(
+                 "C:\\Users\\admin\\source\\repos\\ModuleSixApp2\\ТестКейс.docx",
+                 1,
+                 1,
+                 DataTextBlock.Text);
+
+                ResultTextBlock.Text = "Данные ФИО успешно прошли валидацию!";
+                ResultTextBlock.Foreground = Brushes.Green;
+            }    
         }
     }
 }
